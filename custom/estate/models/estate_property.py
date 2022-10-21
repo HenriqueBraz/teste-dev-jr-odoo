439c21232e2b94f0fe3af6e8269a3f44565bcf81
HenriqueBraz <hbraz9@gmail.com>
2022-10-20 21:13:55 -0300

    chapter 6 completed

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -2,6 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+import datetime
+
 
 class EstateModel(models.Model):
     _name = "estate.property"
@@ -10,14 +13,18 @@ class EstateModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: datetime.date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Type',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_orientation = fields.Selection(required=True, selection=[('any', 'Any'), ('north', 'North'),
+                                                                    ('south', 'South'), ('east', 'East'),
+                                                                    ('west', 'West')], default='any')
+    state = fields.Selection(required=True, selection=[('new', 'New'), ('offer', 'Offer'), ('received', 'Received'),
+                                                       ('offer', 'Offer'), ('accepted', 'Accepted'), ('sold', 'Sold'),
+                                                       ('canceled', 'Canceled')], default='new')
